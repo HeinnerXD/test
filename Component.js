0dd5af6610ad9731ae67a207e5c6b62088d2e5e0
HeinnerXD <heinner45@gmail.com>
2021-01-13 11:44:46 -0500

    other commit

--- Component.js
+++ Component.js
@@ -1,4 +1,4 @@
 /*
  * Copyright (C) 2009-2016 SAP SE or an SAP affiliate company. All rights reserved.
  */
-jQuery.sap.declare("publicservices.her.mycourses.s1.Component");jQuery.sap.require("sap.ca.scfld.md.ComponentBase");sap.ca.scfld.md.ComponentBase.extend("publicservices.her.mycourses.s1.Component",{metadata:sap.ca.scfld.md.ComponentBase.createMetaData("MD",{"manifest":"json",viewPath:"publicservices.her.mycourses.s1.view"}),createContent:function(){var v={component:this};return sap.ui.view({viewName:"publicservices.her.mycourses.s1.Main",type:sap.ui.core.mvc.ViewType.XML,viewData:v});}});
+jQuery.sap.declare("publicservices.her.mycourses.s1.Component");jQuery.sap.require("sap.ca.scfld.md.ComponentBase");sap.ca.scfld.md.ComponentBase.extend("publicservices.her.mycourses.s1.Component",{metadata:sap.ca.scfld.md.ComponentBase.createMetaData("MD",{"manifest":"json",viewPath:"publicservices.her.mycourses.s1.view"}),createContent:function(){var v={component:this};return sap.ui.view({viewName:"publicservices.her.mycourses.s1.Main",type:sap.ui.core.mvc.ViewType.XML,viewData:v});}}); 
